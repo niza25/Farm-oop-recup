2abba9da89476593a179fbdf0f2b931b7950771f
Izabela <izabela.nowosada@gmail.com>
2019-01-16 12:33:00 +0100

    add income method

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+const { Wheat, Crop, Sugarcane } = require('./crops');
+
 class Farm {
     constructor(name) {
-        this.name = name;
+        this.name = name,
+            this.crops = []
+    }
+    addCrop(crop) {
+        this.crops.push(crop)
+    }
+    calculateIncome() {
+        return this.crops
+            .map(crop => crop.getYieldInEuros())
+            .reduce((acc, curr)=> acc + curr, 0);
     }
 }
 
--- index.spec.js
+++ index.spec.js
@@ -1,75 +1,96 @@
-const {Farm} = require('./index');
+const { Farm } = require('./index');
 const { Wheat, Crop, Sugarcane } = require('./crops');
 
 
 test('A new Farm can be created', () => {
     const farm = new Farm()
     expect(farm).toBeTruthy()
-  });   
+});
 
-  test('A Farm can have a name', () => {
+test('A Farm can have a name', () => {
     const farm = new Farm('TEST_NAME')
     expect(farm.name).toBe('TEST_NAME')
-  });
+});
 
-  // Wheat crops test
+// Wheat crops test
 
-  test('The Wheat class can calculate a yield', () => {
+test('The Wheat class can calculate a yield', () => {
     const wheat = new Wheat(100)
     expect(wheat.getYieldInKg()).toBeDefined()
-  });
-  
-  test('The Wheat class calculates the proper yield', () => {
+});
+
+test('The Wheat class calculates the proper yield', () => {
     const wheat = new Wheat(100)
     expect(wheat.getYieldInKg()).toBeCloseTo(674.4, 2)
-  });
+});
 
-  // Sugarcane crops test
-  test('The Sugarcane class can calculate a yield', () => {
+// Sugarcane crops test
+test('The Sugarcane class can calculate a yield', () => {
     const sugarcane = new Sugarcane(100)
     expect(sugarcane.getYieldInKg()).toBeDefined()
-  });
-  
-  /* test('The Sugarcane class calculates the proper yield', () => {
-    const sugarcane = new Sugarcane(100)
-    expect(sugarcane.getYieldInKg()).toBeCloseTo(674.4, 2)
-  }); */
+});
 
-  test('A crop can store its area size', () => {
+/* test('The Sugarcane class calculates the proper yield', () => {
+  const sugarcane = new Sugarcane(100)
+  expect(sugarcane.getYieldInKg()).toBeCloseTo(674.4, 2)
+}); */
+
+test('A crop can store its area size', () => {
     const crop = new Crop(12345)
     expect(crop.acres).toBe(12345)
-  });
+});
 
-  // tests for yield in euros
+// tests for yield in euros
 
 test('Wheat class calculates the proper yield value', () => {
     const wheat = new Wheat(100)
     expect(wheat.getYieldInEuros()).toBeCloseTo(1011.6, 1)
-  })
-  
-  test('Sugarcane class calculates the proper yield value', () => {
+})
+
+test('Sugarcane class calculates the proper yield value', () => {
     const sugarcane = new Sugarcane(200)
     expect(sugarcane.getYieldInEuros()).toBeCloseTo(1943.7, 1)
-  })
-  
-  test('Sugarcane class calculates the proper yield value', () => {
+})
+
+test('Sugarcane class calculates the proper yield value', () => {
     const sugarcane = new Sugarcane(0)
     expect(sugarcane.getYieldInEuros()).toBe(0)
-  })
+})
 
-  // tests for costs
+// tests for costs
 
-  test('Sugarcane class calculates the proper costs value', () => {
+test('Sugarcane class calculates the proper costs value', () => {
     const sugarcane = new Sugarcane(0)
     expect(sugarcane.getCosts()).toBe(0)
-  })
-  
-  test('Sugarcane class calculates the proper costs value', () => {
+})
+
+test('Sugarcane class calculates the proper costs value', () => {
     const sugarcane = new Sugarcane(456)
     expect(sugarcane.getCosts()).toBe(75696)
-  })
-  
-  test('Wheat class calculates the proper costs value', () => {
+})
+
+test('Wheat class calculates the proper costs value', () => {
     const wheat = new Wheat(123)
     expect(wheat.getCosts()).toBe(41820)
+})
+
+// test add crops to farm
+
+test('We can add a crop to our farm', () => {
+    const farm = new Farm('TEST_NAME')
+    farm.addCrop(new Wheat(100))
+  })
+
+  // tests the output of the income method
+
+  test('The income of an empty farm is 0', () => {
+    const farm = new Farm('TEST_NAME')
+    expect(farm.calculateIncome()).toBe(0)
+  })
+
+  test('The income of a 100 Wheat + 100 Sugarcane farm is X', () => {
+    const farm = new Farm('TEST_NAME')
+    farm.addCrop(new Wheat(100))
+    farm.addCrop(new Sugarcane(100))
+    expect(farm.calculateIncome()).toBeCloseTo(1918.37, 1)
   })
